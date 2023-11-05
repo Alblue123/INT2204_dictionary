007eafaff4f7866698998133e95ab1db3555f0b8
Nguyễn Thành Đạt <conchogio012@gmail.com>
2023-11-05 13:39:39 +0700

    updating game base

--- src/main/java/application/controller/GameController.java
+++ src/main/java/application/controller/GameController.java
@@ -9,19 +9,95 @@ public class GameController {
 
     @FXML
     private AnchorPane game_view;
-    private WordyGame wordygame;
+    private WordyGame wordyGame; 
+
+    public GameController() {
+        wordyGame = new WordyGame(5, 5); 
+    }
 
     /** load game view. */
-    public void loadWordyView() {
+    public void loadWordView() {
         try {
             FXMLLoader fxmlLoader = new FXMLLoader();
             fxmlLoader.setLocation(getClass().getResource("/fxml/game_view.fxml"));
             VBox wordBox = fxmlLoader.load();
             game_view.getChildren().addAll(wordBox);
-        
-            wordygame = new WordyGame(wordBox);
+
+            GameView gameViewController = fxmlLoader.getController();
+            gameViewController.setWordyGame(wordyGame);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
+    @FXML
+    public void startNewGame(ActionEvent event) {
+    // Handle starting a new game. You can reset the game state, initialize the board, and start the timer here.
+        wordyGame.resetGameBoard();
+        wordyGame.startTimer();
+}
+    @FXML
+    public void submitWord(ActionEvent event) {
+    // Get the submitted word from the user interface.
+        String submittedWord = wordyGame.getUserInputForWordSubmission();
+
+        if (wordyGame.isWordValid(submittedWord)) {
+            int wordScore = wordyGame.calculateScore(submittedWord);
+        // Update the UI to display the word's score.
+            updateScoreLabel(wordScore);
+        // Add the word to the list of submitted words.
+            wordyGame.addSubmittedWord(submittedWord);
+        } else {
+        // Handle an invalid word (e.g., display an error message).
+            displayInvalidWordError();
+        }
+    }
+
+    @FXML
+    private Label scoreLabel; 
+    
+    public void updateScoreLabel(int score) {
+        scoreLabel.setText("Score: " + score);
+    }
+    @FXML
+    private GridPane gameBoardGrid;
+
+    public void updateGameBoard(char[][] board) {
+        int numRows = board.length;
+        int numCols = board[0].length;
+    
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j < numCols; j++) {
+                // Assuming you have Text elements in your GridPane with corresponding row and column indices.
+                // You can access them using the getChildren() method.
+                Text textElement = (Text) gameBoardGrid.getChildren().get(i * numCols + j);
+    
+                // Set the text of the Text element to the letter from the 'board' array.
+                textElement.setText(Character.toString(board[i][j]));
+            }
+        }
+    }
+    @FXML
+    private Button shuffleButton;
+    
+    @FXML
+    public void shuffleBoard(ActionEvent event) {
+        wordyGame.shuffleBoard();
+        updateGameBoard(wordyGame.getBoard());
+    }
+    
+
+    public void displayGameOutcomeAndScore(String outcome, int finalScore) {
+        Alert alert = new Alert(AlertType.INFORMATION);
+        alert.setTitle("Game Outcome");
+        alert.setHeaderText("Game Over");
+        alert.setContentText("Outcome: " + outcome + "\nFinal Score: " + finalScore);
+        alert.showAndWait();
+    }
+
+    public void resetUIForNewGame() {
+        scoreLabel.setText("Score: 0"); // Reset the score label to its initial state.
+        // Reset the game board UI elements as needed.
+    }
+    
+    
 }
--- src/main/java/application/controller/WordyGame.java
+++ src/main/java/application/controller/WordyGame.java
@@ -1,3 +1,173 @@
+import java.util.Random;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Timer;
+import java.util.TimerTask;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+
 public class WordyGame {
+    private char[][] board;
+    private int rows;
+    private int cols;
+    private Random random = new Random();
+    private Set<String> validWords = new HashSet<>(); // A set of valid words from a dictionary.
+    private int score = 0;
+
+    public WordyGame(int rows, int cols) {
+        this.rows = rows;
+        this.cols = cols;
+        this.board = new char[rows][cols];
+
+        this.timer = new Timer();
+        this.elapsedTime = 0;
+    }
+
+    public void initializeBoard() {
+        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                int randomIndex = random.nextInt(alphabet.length());
+                char randomLetter = alphabet.charAt(randomIndex);
+                board[i][j] = randomLetter;
+            }
+        }
+    }
+
+    public char[][] getBoard() {
+        return board;
+    }
+
+    public boolean isWordValid(String word) {
+        return validWords.contains(word);
+    }
+
+    public String getUserInputForWordSubmission() {
+        // You can use a TextField or any other JavaFX input control to get user input.
+        // Assuming you have a TextField named 'wordInputField' in your FXML file.
+        String userInput = wordInputField.getText();
+        return userInput;
+    }
+
+    public char getLetterAt(int row, int col) {
+        // Check if the row and column indices are valid.
+        if (row >= 0 && row < rows && col >= 0 && col < cols) {
+            return board[row][col];
+        } else {
+            // Handle invalid indices (e.g., return a default character or throw an exception).
+            return ' '; // Replace with an appropriate default value.
+        }
+    }
+
+    public void shuffleBoard() {
+        // Create a copy of the current game board.
+        char[][] newBoard = new char[rows][cols];
     
+        // Create a list of all characters from the current board.
+        List<Character> characters = new ArrayList<>();
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                characters.add(board[i][j]);
+            }
+        }
+    
+        // Shuffle the list of characters.
+        Collections.shuffle(characters);
+    
+        // Populate the new board with shuffled characters.
+        int index = 0;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                newBoard[i][j] = characters.get(index);
+                index++;
+            }
+        }
+    
+        // Update the game board with the shuffled board.
+        board = newBoard;
+    }
+    
+    public int calculateScore(String word) {
+
+        int wordLength = word.length();
+        if (wordLength < 3) {
+            return 1;
+        } else if (wordLength < 5) {
+            return 2;
+        } else if (wordLength < 7) {
+            return 3;
+        } else {
+            return 4;
+        }
+    }
+    public int calculateFinalScore(List<String> submittedWords) {
+        int finalScore = 0;
+    
+        for (String word : submittedWords) {
+            if (isWordValid(word)) {
+                int wordScore = calculateScore(word);
+                finalScore += wordScore;
+                score += wordScore;
+            }
+        }
+    
+        return finalScore;
+    }
+
+
+    public String determineGameOutcome(int finalScore) {
+        if (finalScore >= 50) {
+            return "You won! Great job!";
+        } else if (finalScore >= 25) {
+            return "You did well. It's a tie!";
+        } else {
+            return "You lost. Try again!";
+        }
+    }
+
+    public void displayGameOutcomeAndScore(String gameOutcome, int finalScore) {
+        Alert alert = new Alert(AlertType.INFORMATION);
+        alert.setTitle("Game Outcome");
+        alert.setHeaderText("Game Over");
+        alert.setContentText("Outcome: " + gameOutcome + "\nFinal Score: " + finalScore);
+        alert.showAndWait();
+    }
+    
+    public void resetGameBoard() {
+        score = 0;
+        submittedWords.clear();
+        resetTimer();
+        initializeBoard();
+        gameInProgress = false;
+        
+    }
+    public void startTimer() {
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                elapsedTime++;
+            }
+        }, 1000, 1000);
+    }
+
+    public void stopTimer() { 
+        timer.cancel(); 
+    }
+
+    public void resetTimer() {
+        timer.cancel();
+        elapsedTime = 0; 
+    }
+
+
+    public int getElapsedTime() { 
+        return elapsedTime;
+    }
+    public void endGame() {
+        int finalScore = calculateFinalScore();
+        String gameOutcome = determineGameOutcome(finalScore);
+        displayGameOutcomeAndScore(gameOutcome, finalScore);
+        resetGameBoard();
+    }
 }
