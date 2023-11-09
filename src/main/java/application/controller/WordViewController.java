bdf217208cae734f9a4d81d3865d5b255724a951
Alblue123 <87468255+Alblue123@users.noreply.github.com>
2023-11-09 08:44:34 +0700

    delete and speak for word

--- src/main/java/application/controller/WordViewController.java
+++ src/main/java/application/controller/WordViewController.java
@@ -1,5 +1,7 @@
 package application.controller;
 
+import application.API.VoiceRSS;
+import application.backCode.Trie;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
@@ -8,6 +10,8 @@ import javafx.scene.control.TreeView;
 import javafx.scene.web.WebView;
 import javafx.scene.input.MouseEvent;
 
+import static application.MainApplication.dictionary;
+
 public class WordViewController extends MasterView {
     @FXML
     private WebView word_view_web_view;
@@ -15,6 +19,10 @@ public class WordViewController extends MasterView {
     private Label view_word_word;
     @FXML
     private Label book_mark;
+    @FXML
+    private Label word_delete;
+    @FXML
+    private Label word_speaker;
 
 
     public void init(String target, String explain) {
@@ -22,9 +30,28 @@ public class WordViewController extends MasterView {
         word_view_web_view.getEngine().loadContent(explain, "text/html");
     }
 
-    public void selectItem() {
-        //TreeItem<String> item = treeView.getSelectionModel().getSelectedItem();
+    @FXML
+   public void clickDelete(MouseEvent e) {
+        String word = view_word_word.getText();
+        dictionary.delete(word);
+        Trie.erase(word);
+        this.reload();
     }
+
+    public void reload() {
+        view_word_word.setText("");
+        word_view_web_view.getEngine().loadContent("", "text/html");
+    }
+
+    @FXML
+    public void clickSpeaker(MouseEvent e) throws Exception {
+        String word = view_word_word.getText();
+        if (word != null) {
+            VoiceRSS.speakWords(word, "Nancy", "en-gb");
+        }
+
+    }
+
 
     @FXML
     public void clickBookMark(MouseEvent e) {
