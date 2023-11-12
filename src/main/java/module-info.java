f0e4e552c493d1e92e02b55ea61a0dafe312d42b
Nguyễn Thành Đạt <conchogio012@gmail.com>
2023-11-12 22:57:45 +0700

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,14 @@ module application {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.web;
+    requires java.sql;
+    requires org.xerial.sqlitejdbc;
+    requires voicerss.tts;
+    requires javafx.media;
+    requires java.desktop;
+    requires org.apache.tika.core;
 
     opens application.controller to javafx.fxml;
     exports application;
+    opens application.backCode to javafx.fxml;
 }
